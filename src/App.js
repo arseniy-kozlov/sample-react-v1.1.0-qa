9989eee8e8f4f9bc275afbec8535e7608a5e342d
Yuri Serikov <ys@inmoloko.ru>
2023-01-18 17:23:06 +0500

    gitloc-react-sample initail

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import LanguageSwitcher from "./components/LanguageSwitcher";
+import { useTranslation, Trans } from "react-i18next";
+
+const fileName = "src\\App.js";
 
 function App() {
+  const { t } = useTranslation();
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <LanguageSwitcher />
+        <h1>{t("title", { ns: "welcome" })}</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Trans i18nKey="description.text">
+            <code>{{ fileName }}</code>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {t("description.reactLink")}
+            </a>
+          </Trans>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
