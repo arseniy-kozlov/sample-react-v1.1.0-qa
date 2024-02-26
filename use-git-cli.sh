097052ccc8d7a8f3b610ea523952a1dee6a93c60
Arsenii Kozlov <arseniy.perm@gmail.com>
2024-02-26 16:30:04 +0500

    clean

---
+++ use-git-cli.sh
@@ -0,0 +1,13 @@
+> git push origin main
+To https://github.com/gitloc-org/sample-react.git
+   5480f05..080b3da  main -> main
+
+> git pull origin main
+From https://github.com/gitloc-org/sample-react.git
+ * branch            main       -> FETCH_HEAD
+   080b3da..6e5d3ba  main       -> origin/main
+Updating 080b3da..6e5d3ba
+Fast-forward
+ public/locales/de/test.json | 2 +-
+ public/locales/es/test.json | 2 +-
+ 2 files changed, 2 insertions(+), 2 deletions(-)
